11e115a3dbccdd2308e164643ee1a756d295ea0e
Tapha Ngum <tapha.ngum@qa.com>
2022-09-07 13:49:49 +0100

    [up-1] added webpack to project

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // const topNavBtn = document.getElementsByClassName('top-nav-btn');
 // const topNavLinks = document.getElementsByClassName('top-nav-nav');
-let topNavLinks = document.getElementById('top-nav-id');
+// let topNavLinks = document.getElementById('top-nav-id');
 
-const toggleStates = () => {
-    // toggle between visible and hidden
-    if (topNavLinks.style.display === 'none') {
-        // Toggle state
-        topNavLinks.style.display = 'block';
-        console.error('Links have appeared.');
-    } else {
-        // Set state to display none
-        topNavLinks.style.display = 'none';
-        console.error('Links have disappeared.');
-    }
-}
+// const toggleStates = () => {
+//     // toggle between visible and hidden
+//     if (topNavLinks.style.display === 'none') {
+//         // Toggle state
+//         topNavLinks.style.display = 'block';
+//         console.error('Links have appeared.');
+//     } else {
+//         // Set state to display none
+//         topNavLinks.style.display = 'none';
+//         console.error('Links have disappeared.');
+//     }
+// }
+
+// React Project
