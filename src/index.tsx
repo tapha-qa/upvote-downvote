7d804d239df6775b7b4c64f654187a3145ea3c5c
Tapha Ngum <tapha.ngum@qa.com>
2022-09-07 15:00:00 +0100

    [up-1] migrating project to react.

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import react from "react";
+import reactDom from "react-dom";
+import "style.scss";
+
+// Setting up a set of components
+
+// Binding to our root element
+const root = reactDom.createRoot(document.getElementById("root"));
